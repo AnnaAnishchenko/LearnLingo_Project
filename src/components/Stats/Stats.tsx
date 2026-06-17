dd67cdc3cbcf5cf2876229d8f7298dcbf658bede
Anna Anishchenko <annuwkaanishchenko@gmail.com>
2026-06-17 11:20:40 +0300

    hero+stats

--- src/components/Stats/Stats.tsx
+++ src/components/Stats/Stats.tsx
@@ -1,6 +1,16 @@
 // scr/Stats/Stats.tsx
+import { stats } from "../../data/stats";
 import css from "./Stats.module.css";
 
 export default function Stats() {
-  return <div className={css.statsBox}></div>;
+  return (
+    <ul className={css.statsBox}>
+      {stats.map(({ value, text }) => (
+        <li key={text} className={css.statsItem}>
+          <span className={css.value}>{value}</span>
+          <span className={css.text}>{text}</span>
+        </li>
+      ))}
+    </ul>
+  );
 }
---
+++ src/data/stats.ts
@@ -0,0 +1,18 @@
+export const stats = [
+  {
+    value: "32,000 +",
+    text: "Experienced tutors",
+  },
+  {
+    value: "300,000 +",
+    text: "5-star tutor reviews",
+  },
+  {
+    value: "120 +",
+    text: "Subjects taught",
+  },
+  {
+    value: "200 +",
+    text: "Tutor nationalities",
+  },
+];
